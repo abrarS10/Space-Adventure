a33ddeebf8197dece003936f601a6c3519b68e2d
abrar_sajeel <abrarsajeel@gmail.com>
2022-09-27 18:52:50 +0500

    Added pause menu

---
+++ Space Adventure/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    
+    public static bool isPaused = false;
+    public GameObject pauseMenu;
+
+    void Start(){
+        pauseMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            if (isPaused){
+                Resume();
+            } else {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume(){
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void Pause(){
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void loadMenu(){
+        //TODO Add menu scene
+        Debug.Log("Loading menu");
+    }
+}

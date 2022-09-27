a0f7933973eb046bd267ee29db4cc53e306a1f41
abrar_sajeel <abrarsajeel@gmail.com>
2022-09-27 11:58:58 +0500

    Added interaction with rocket to end game

--- Space Adventure/Assets/Scripts/PlayerInventory.cs
+++ Space Adventure/Assets/Scripts/PlayerInventory.cs
@@ -1,10 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerInventory : MonoBehaviour
 {
     public int materialCount = 0;
+    public const int requiredMaterials = 2;
+    public bool triggerActive = false;
+    void OnTriggerEnter2D(Collider2D other){
+        if (other.tag == "Interactable"){
+            triggerActive = true;
+        }
+    }
 
+    void OnTriggerExit2D(Collider2D other){
+        if (other.tag == "Interactable"){
+            triggerActive = false;
+        }
+    }
 
+    void OnInteract(InputValue input){
+        if(triggerActive && input.isPressed){
+            if(materialCount >= requiredMaterials){
+                Debug.Log("You won!!!");
+            } else {
+                Debug.Log("Not enough materials");
+            }
+        }
+           
+    }
 }
